cceec4d4d1448214fe649929dfc7deaf04868653
bissaye <bissaye.com@gmail.com>
2021-09-30 23:39:58 +0100

    creation du projet

---
+++ syscab_api/core/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+class TestView(APIView):
+    def get(self, request, *args, **kwargs):
+        data = {
+            'name':'john',
+            'age': 23
+        }
+        return Response(data)
+
+
+# def test_view(request):
+#     data = {
+#         'name':'john',
+#         'age': 23
+#     }
+#     return JsonResponse(data)
